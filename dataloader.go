98dd729c32d5a12fcdc9435e65514c2856446f20
Preston Wagner <pwag42@hotmail.com>
2023-02-21 09:56:23 -0700

    WIP

--- dataloader.go
+++ dataloader.go
@@ -8,9 +8,8 @@ import (
 )
 
 type batch[KEY_TYPE comparable, VALUE_TYPE any] struct {
-	channelCache map[KEY_TYPE][]chan VALUE_TYPE
-	maxBatchSize int
-	maxBatchWait time.Duration
+	queries map[KEY_TYPE]unicycle.Promise[VALUE_TYPE]
+	start   time.Time
 }
 
 // A getter function accepts a list of de-duplicated keys, and returns a pair of maps from keys to values (for successful lookups) and keys to errors (for unsuccessful lookups)
@@ -45,7 +44,7 @@ func (dataloader *DataLoader[KEY_TYPE, VALUE_TYPE]) QueueRequest(key KEY_TYPE) V
 	// }
 }
 
-func (dataloader *DataLoader[KEY_TYPE, VALUE_TYPE]) Load(key KEY_TYPE) VALUE_TYPE {
+func (dataloader *DataLoader[KEY_TYPE, VALUE_TYPE]) Load(key KEY_TYPE) (VALUE_TYPE, error) {
 
 	// for {
 	// 	select {
@@ -55,4 +54,26 @@ func (dataloader *DataLoader[KEY_TYPE, VALUE_TYPE]) Load(key KEY_TYPE) VALUE_TYP
 	// 		return
 	// 	}
 	// }
+}
+
+type DataLoaderCached[KEY_TYPE comparable, VALUE_TYPE any] struct {
+	loader     DataLoader[KEY_TYPE, VALUE_TYPE]
+	valueCache map[KEY_TYPE]VALUE_TYPE
+	lock       *sync.RWMutex
+}
+
+func (dataLoaderCached *DataLoaderCached[KEY_TYPE, VALUE_TYPE]) Load(key KEY_TYPE) (VALUE_TYPE, error) {
+	dataLoaderCached.lock.RLock()
+	value, ok := dataLoaderCached.valueCache[key]
+	dataLoaderCached.lock.RUnlock()
+	if ok {
+		return value, nil
+	}
+	value, err := dataLoaderCached.loader.Load(key)
+	if err == nil {
+		dataLoaderCached.lock.Lock()
+		dataLoaderCached.valueCache[key] = value
+		dataLoaderCached.lock.Unlock()
+	}
+	return value, err
 }
